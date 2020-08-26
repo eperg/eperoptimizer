9351a0ba319e267686cfff8593bbc5cf9edae6e6
Eper Gaboni <hi@epergaboni.com>
2020-08-27 07:38:59 +0800

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import "<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">";
 
 ReactDOM.render(<App />, document.getElementById("root"));
